f10abaaeae2cff3eb21bff753f3933400255a680
katsiaryna-volkava <volkava.katsiaryna@gmail.com>
2019-10-03 19:33:35 +0300

    implementation

---
+++ src/test/java/model/Company.java
@@ -0,0 +1,46 @@
+package model;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Company {
+
+    private String name;
+    private String catchPhrase;
+    private String bs;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCatchPhrase() {
+        return catchPhrase;
+    }
+
+    public void setCatchPhrase(String catchPhrase) {
+        this.catchPhrase = catchPhrase;
+    }
+
+    public String getBs() {
+        return bs;
+    }
+
+    public void setBs(String bs) {
+        this.bs = bs;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ src/test/java/model/Geo.java
@@ -0,0 +1,37 @@
+package model;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Geo {
+
+    private String lat;
+    private String lng;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
